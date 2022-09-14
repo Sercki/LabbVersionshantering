8d8a234bf2b0aee9cdbc605379397b5fca8a3969
Sergiusz <sergiusz.radecki@gmail.com>
2022-09-14 20:53:37 +0200

    created new branch med new feature

--- exempel/exempel/Program.cs
+++ exempel/exempel/Program.cs
@@ -1,8 +1,11 @@
 ﻿Duck Anka = new Duck(2, 2, "bird");
 Duck Hund = new Dog(4, 0, "dog");
+Duck Fisk = new Fish(0, 0, 6, "fish");
 Anka.run();
 Console.WriteLine("");
 Hund.run();
+Console.WriteLine("");
+Fisk.run();
 
 class Duck
 {
@@ -51,4 +54,26 @@ class Dog : Duck
     {
         Console.WriteLine($"I cannot fly because I have {wings} wings.");
     }
+}
+
+class Fish : Duck
+{
+    int fins;
+    public Fish(int l, int w, int f, string i) : base(l, w, i)
+    {
+        this.fins = f;
+    }
+    public override void fly()
+    {
+        Console.WriteLine($"I cannot fly because I have {wings} wings.");
+    }
+    public override void walk()
+    {
+        Console.WriteLine($"I cannot walk because I have {legs} legs.");
+    }
+    public override void swim()
+    {
+        Console.WriteLine($"I can swim because I have {fins} fins.");
+    }
+
 }
