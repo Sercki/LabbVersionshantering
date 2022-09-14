0810dad3543ad49d9124784c41dacd686d360315
Sergiusz <sergiusz.radecki@gmail.com>
2022-09-14 20:34:19 +0200

    added a new class: dog

--- exempel/exempel/Program.cs
+++ exempel/exempel/Program.cs
@@ -1,33 +1,54 @@
-﻿Duck Anka = new Duck();
+﻿Duck Anka = new Duck(2, 2, "bird");
+Duck Hund = new Dog(4, 0, "dog");
 Anka.run();
+Console.WriteLine("");
+Hund.run();
 
 class Duck
 {
-    int legs = 2;
-    int wings = 2;
-    string identity = "bird";
+    protected int legs;           
+    protected int wings;          
+    protected string identity;    
 
-    public void fly()
+    public Duck(int l, int w, string i)
+    {
+        this.legs = l;
+        this.wings = w;
+        this.identity = i;
+    }
+    public virtual void fly()
     {
         Console.WriteLine($"I'm using {wings} wings to fly.");
     }
-    public void walk()
+    public virtual void walk()
     {
-        Console.WriteLine($"I'm using {legs} wings to walk.");
+        Console.WriteLine($"I'm using {legs} legs to walk.");
     }
-    public void swim()
+    public virtual void swim()
     {
         Console.WriteLine($"I'm using {legs} legs to swim.");
     }
-    public void identification()
+    public virtual void identification()
     {
         Console.WriteLine($"I'm a {identity}.");
     }
-    public void run()
+    public virtual void run()
     {
         identification();
         fly();
         walk();
         swim();
+    }
+}
+
+class Dog : Duck
+{
+    public Dog(int l, int w, string i) : base(l, w, i)
+    {
+    }
+
+    public override void fly()
+    {
+        Console.WriteLine($"I cannot fly because I have {wings} wings.");
     }
 }
